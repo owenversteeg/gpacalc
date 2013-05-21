206c7a42bac208ac9b39e5d4d8dd2f0db50d71a8
Owen Versteeg <owen.versteeg@live.com>
2013-05-21 13:14:57 -0300

    semicolon
    now it all works onelined

--- gpacalc.js
+++ gpacalc.js
@@ -1,4 +1,4 @@
-javascript:void(function(){var s=document.createElement('script'); s.src='//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'; document.getElementsByTagName('head')[0].appendChild(s);}())
+javascript:void(function(){var s=document.createElement('script'); s.src='//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'; document.getElementsByTagName('head')[0].appendChild(s);}());
 
 window.setTimeout(function() {
 
